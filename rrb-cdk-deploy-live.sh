87bc4d33b5efc61efd92c5001d0936a58e94ae34
Richard R. Bernardy Jr. <rbernard@usf.edu>
2026-02-16 21:59:29 -0500

    fixed the rrb-cdk-deploy-live.sh script to include the unique destination bucket variable

--- rrb-cdk-deploy-live.sh
+++ rrb-cdk-deploy-live.sh
@@ -1,1 +1,1 @@
-cdk deploy PDFAccessibility -c source_bucket=pdfaccessibility-pdfaccessibilitybucket149b7021e-ljzn29qgmwog -c destination_bucket=usflibraries-pdfaccessibility-public-live --require-approval never
+cdk deploy PDFAccessibility -c source_bucket=pdfaccessibility-pdfaccessibilitybucket149b7021e-ljzn29qgmwog -c destination_bucket=usflibraries-pdfaccessibility-live-public --require-approval never
