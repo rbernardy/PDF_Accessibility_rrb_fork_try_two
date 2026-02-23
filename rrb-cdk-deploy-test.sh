08a18422250cfc53d77c3c76eaa3a9cd2dad8454
Richard Bernardy <rbernard@usf.edu>
2026-02-23 11:01:09 -0500

    updated my rrb-cdk-deploy-test.sh script to restore the rm -rf ./cdk.out/ statement which helps force a full rebuild for deploymenmt

--- rrb-cdk-deploy-test.sh
+++ rrb-cdk-deploy-test.sh
@@ -15,4 +15,5 @@ fi
 
 echo ""
 echo "Proceeding with TEST deployment..."
+rm -rf ./cdk.out/
 cdk deploy PDFAccessibility -c source_bucket=pdfaccessibility-pdfaccessibilitybucket149b7021e-ljzn29qgmwog -c destination_bucket=usflibraries-pdfaccessibility-public --require-approval never --force
