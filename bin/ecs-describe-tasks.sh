551a937ba113739a8b69be8f8c856f574417875f
Richard Bernardy <rbernard@usf.edu>
2026-03-02 23:19:59 -0500

    added two new bin/scripts for ecs task info retrieval

---
+++ bin/ecs-describe-tasks.sh
@@ -0,0 +1,1 @@
+aws ecs describe-tasks --cluster $AWS_ECS_CLUSTER_CNAME --tasks "$1" --profile $AWS_PROFILE
---
+++ bin/ecs-list-tasks.sh
@@ -0,0 +1,1 @@
+aws ecs list-tasks --cluster $AWS_ECS_CLUSTER_CNAME --profile $AWS_PROFILE --desired-status RUNNING
